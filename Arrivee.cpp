afe3bfb6debff93b188019c87fa10dfb7d7b1215
Aleksandar VASILJEVIC <aleksandarvasiljevic12@gmail.com>
2023-09-07 15:21:30 +0200

    feat: standard deviation

--- Arrivee.cpp
+++ Arrivee.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 using namespace std;
 #include "Sed.hpp"
 #include "Arrivee.hpp"
@@ -13,10 +14,21 @@ void Arrivee::traiter() {
     Banque* banque = (Banque*) _sed;
     Client *c = new Client(_heure);
     Caissier *cs = banque->unCaissierLibre();
-    if (cs) {
+
+    if (cs)
         cs->servir(*c);
-    } else { banque->fileAttente()->ajouter(*c); }
-    double hpa = _heure + (rand() % (4 - 2 + 1)) + 2; // TODO: faire en sorte de faire une distribution normal with th standard deviation
+    else
+        banque->fileAttente()->ajouter(*c);
+
+    // double hpa = _heure + (rand() % (4 - 2 + 1)) + 2; // TODO: faire en sorte de faire une distribution normal with th standard deviation
+    double tempsMoyenService = 5;
+    double ecartTypePercentage = 10;
+    double ecartType = (tempsMoyenService/100) * ecartTypePercentage;
+    random_device rd;
+    mt19937 generator(rd());
+    normal_distribution<double> distribution(tempsMoyenService, ecartType);
+    double hpa = distribution(generator);
+
     if (hpa <= banque->dureePrevue()) {
         Arrivee *a = new Arrivee(banque, hpa);
         banque->ajouter(*a);
